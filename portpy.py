7490adc5f436119d96a1374af1d19021cbf4ffa2
Ericson Willians Rocha Pires <ericsonwrp@gmail.com>
2015-09-28 21:34:04 -0300

    Serializable, Data and Language were added.

--- portpy.py
+++ portpy.py
@@ -1,25 +1,80 @@
-# -*- coding: utf-8 -*-
+import codecs
+from sys import argv
+from json import dumps, load
+from os import path
 
-import sys
+DEFAULT_LANGUAGE_PATH = "lang"
 
+class Serializable(object):
 
-dicionario = [
-    ['False', 'Falso'], ['None', 'Nada'], ['True', 'Verdadeiro'],
-    ['as', 'como'], ['assert', 'afirmar'], ['break', 'quebrar'],
-    ['class', 'classe'], ['continue', 'continuar'], ['def', 'func'], ['del', 'excluir'],
-    ['elif', 'senão se'], ['else', 'senão'], ['except', 'exceto'], ['finally', 'finalmente'],
-    ['for ', 'para '], ['from ', 'de '], ['if ', 'se '], ['if(', 'se('], ['import', 'importar'], [' in ', ' em '],
-    [' is ', ' é '], ['nonlocal', 'nãolocal'], [' not ', ' não '], [' or ', ' ou '], ['pass', 'passar'],
-    ['raise', 'levantar'], ['return', 'retornar'], ['try', 'tentar'], ['while', 'enquanto'],
-    ['with', 'com'], ['print', 'escrever', 'imprimir'], [' and ', ' e ']
-    ]
+	def __init__(self):
+		
+		self.file = None
+		
+	def serialize(self, path, mode):	
+		try:
+			self.file = open(path, mode)
+		except:
+			raise FileNotFoundError()
+		if self.file is not None:
+			return self.file
+			self.file.close()
+			
+class Data(Serializable):
 
-with open(sys.argv[1], 'r') as arquivo:
-    texto = arquivo.read()
-    for keyword in dicionario:
-        if len(keyword) <= 2:
-            texto = texto.replace(keyword[1], keyword[0])
-        else:  # se tiver mais de uma palavra
-            for i in range(1, len(keyword)):
-                texto = texto.replace(keyword[i], keyword[0])
-    exec(texto)
+	def __init__(self):
+		Serializable.__init__(self)
+		self.data = {}
+		
+	def __setitem__(self, key, value):
+		self.data[key] = value
+		
+	def __getitem__(self, key):
+		return self.data[key]
+		
+	def get_data(self):
+		return self.data
+		
+	def write(self, path):
+		self.serialize(path, "w").write(dumps(self.get_data()))
+		
+	def load(self, path):
+		# AHHHHHHHHHHHHHHHHHHH, foda-se essa merda de encoding (Por enquanto).
+		with codecs.open(path, "r", encoding="utf-8", errors="replace") as data: 
+			self.data = load(data)
+		
+		return self.data
+
+class Language(Data):
+	
+	def __init__(self, name):
+		Data.__init__(self)
+		self.name = name
+		self.load(path.join(DEFAULT_LANGUAGE_PATH, self.name + ".txt"))
+
+if __name__ == "__main__":
+
+	if len(argv) > 1:
+		lang_name = argv[1]
+		script_path = argv[2]
+		lang = Language(lang_name)
+		with open(script_path, 'r') as _file:
+			script = _file.read()
+			for item in lang.data.items():
+				if isinstance(item[1], list):
+					for e in item[1]:
+						script = script.replace(e, item[0])
+				elif isinstance(item[1], str):
+					script = script.replace(item[1], item[0])
+		
+		#Feio, muito feio. "Parsearei" melhor em breve.
+		try:
+			exec(script)
+		except SyntaxError:
+			try:
+				exec(script.replace("elif if", "elif"))
+			except SyntaxError:
+				exec(script.replace("else if", "elif"))
+		
+	else:
+		raise Exception("Please provide the name of the language file. If the file is cz.txt, for example, than type just 'cz'.")
